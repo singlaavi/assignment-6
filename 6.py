06fdf7c4e5079a0e0c1d6a02013d287f44ba96a2
singlaavi <avi.singla16@gmail.com>
2018-08-22 22:17:38 +0530

    assignment 6

---
+++ 6.py
@@ -0,0 +1,45 @@
+#Q.1-Calculate Area of a Sphere
+def sphere(radius):
+    area=4*3.14*radius*radius
+    print(area)
+
+radius=int(input('Enter Radius:'))
+sphere(radius
+       )
+
+#Q.2-Prints All the Perfect Numbers Between 1 and 1000
+
+def perfect(num):
+    sum=0
+    i=1
+    while i<num:
+        if(num%i==0):
+            sum=sum+i
+        i+=1
+        
+    if num!=1 and sum==num:
+         print (num)
+         
+print('The Perfect Numbers are:')
+for i in range (1,1000):
+    perfect(i)
+
+#Q.3-Print Multiplication Table of a User Defined Number
+def multiplication():
+    n=int(input('Enter integer:'))
+    for i in range(1,11):
+        print(n,'x',i,'=',n*i)
+
+multiplication()
+    
+#Q.4-Calculate Power of a Number Using Recursion
+def exponent(base,exp):
+    
+    if(exp==1):
+        return base
+    else:
+        return base*exponent(base,exp-1)
+base=int(input("Enter base: "))
+exp=int(input("Enter exponential number: "))
+a=exponent(base,exp)
+print(a)
